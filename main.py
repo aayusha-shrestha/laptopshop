8f22eb6e14c28f34efdea09db2b18198b3b668b0
Aayusha <aayyushashrestha@gmail.com>
2023-04-28 12:14:00 +0545

    added cart

--- main.py
+++ main.py
@@ -8,21 +8,21 @@ print("\t\t\t\t\t\t" + "Buy the best laptops at the best price."+"\n")
 print("--------------------------------------------------------------------------------------------------------------------------------------\n")
 
 laptops_2d_lst = []
-laptop_file = open("availableLaptops.txt","r")
-for line in laptop_file:
-    inner_lst = line.split(",") #split the line on runs of commas
-    laptops_2d_lst.append(inner_lst)
-laptop_file.close()
+with open("availableLaptops.txt","r") as laptop_file:
+    for line in laptop_file:
+        inner_lst = line.split(",") #split the line on runs of commas
+        laptops_2d_lst.append(inner_lst)
 
 def display_laptops():
     """reads text file containing laptops and displays it"""
-    laptop_file = open("availableLaptops.txt","r")
-    print("LAPTOP ID\t"+"NAME\t\t\t"+"BRAND\t\t\t"+"PRICE\t\t"+"QUANTITY\t"+"PROCESSOR\t\t"+"GRAPHIC CARD"+"\n")
-    laptop_id = 1
-    for line in laptop_file:
-        print(laptop_id,"\t\t" + line.replace(",","\t\t"))
-        laptop_id += 1
-    laptop_file.close()
+    with open("availableLaptops.txt","r") as laptop_file:
+        print("LAPTOP ID\t"+"NAME\t\t\t"+"BRAND\t\t\t"+"PRICE\t\t"+"QUANTITY\t"+"PROCESSOR\t\t"+"GRAPHIC CARD"+"\n")
+        print("--------------------------------------------------------------------------------------------------------------------------------------\n")
+        laptop_id = 1
+        for line in laptop_file:
+            print(laptop_id,"\t\t" + line.replace(",","\t\t"))
+            laptop_id += 1
+        print("\n"+"--------------------------------------------------------------------------------------------------------------------------------------\n")
 display_laptops()
 
 def display_operation_num():
@@ -36,38 +36,60 @@ while user_input:
     input_number = input("Enter the number of your choice for operation: ")
     if input_number == "1":
         #ask for personal info
+        print("\n")
         customer_name = input("Enter your name/company's name: ")
+        print("\n")
+        print("Hi "+customer_name+"! Welcome to Techno Zone."+"\n"+"Take a look at our laptops and decide which one you want."+"\n"+
+              "Before proceeding further we will be asking for your personal details for the bill.")
+        print("\n")
         customer_contact = input("Enter your contact no.: ")
+        print("\n")
         customer_address = input("Enter your address: ")
+        print("\n")
     
+        cart = []
         def select_laptop():
             #ask for laptop id
             customer_laptop_id = int(input("Enter laptop id of the laptop you would like to purchase: "))
+            print("\n")
             while customer_laptop_id <= 0 or customer_laptop_id > len(laptops_2d_lst):
-                print("\n"+"INVALID INPUT"+"\n")
+                print("INVALID INPUT"+"\n")
                 display_laptops()
                 customer_laptop_id = int(input("Enter laptop id of the laptop you would like to purchase: "))
+                print("\n")
 
             #update laptop quantity in 2d list
             customer_qty = int(input("Enter the number of laptops you would you like to purchase: "))
+            print("\n")
             current_qty = int(laptops_2d_lst[customer_laptop_id-1][3])
             while customer_qty <= 0 or customer_qty > current_qty:
-                print("\n"+"INVALID QUANTITY"+"\n")
+                print("INVALID QUANTITY"+"\n")
                 customer_qty = int(input("Enter the number of laptops you would you like to purchase: "))
+                print("\n")
             laptops_2d_lst[customer_laptop_id-1][3] = str(current_qty - customer_qty)
 
             #update laptop quantity in txt file
-            laptop_file = open("availableLaptops.txt","w")
-            for list in laptops_2d_lst:
-                list_to_string = ','.join(list)
-                laptop_file.write(list_to_string)
-            laptop_file.close()
+            with open("availableLaptops.txt","w") as laptop_file:
+                for list in laptops_2d_lst:
+                    list_to_string = ','.join(list)
+                    laptop_file.write(list_to_string)
+            
+            #cart
+            customer_product = laptops_2d_lst[customer_laptop_id-1][0]
+            cost_price_dollar = laptops_2d_lst[customer_laptop_id-1][2]
+            cost_price = laptops_2d_lst[customer_laptop_id-1][2].replace("$","")
+            total_price = int(cost_price)*customer_qty
+
+            cart.append([customer_product,customer_qty,cost_price_dollar,total_price])
+            print(cart)
+            return customer_laptop_id,customer_qty
         select_laptop()
 
         #ask if more laptop needed
         buy_more = True
         while buy_more: 
             user_answer = input("Do you want to buy more?(y/n): ")
+            print("\n")
             if user_answer == "y":
                 display_laptops()
                 select_laptop()
@@ -75,14 +97,14 @@ while user_input:
                 ship = True
                 while ship:
                     ship_or_not = input("Do you want to ship?(y/n): ")
+                    print("\n")
                     if ship_or_not == "y":
-                        print("shipped")
                         ship = False
                     elif ship_or_not == "n":
                         print("not shipped")
                         ship = False
                 buy_more = False     
-        #break loop
+        #break main loop
         user_input = False
     elif input_number == "3":
         exit()
