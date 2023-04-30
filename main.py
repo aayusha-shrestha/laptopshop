f86b2ce0470d6c6f8953153f46e8d0cb63cd3945
Aayusha <aayyushashrestha@gmail.com>
2023-04-30 18:08:07 +0545

    completed bill

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from operations import welcome_msg,display_laptops,display_operation_num,personal_info,select_laptop
+from operations import welcome_msg,display_laptops,display_operation_num,personal_info,select_laptop,bill
 
 welcome_msg()
 
@@ -11,7 +11,6 @@ user_input = True
 while user_input:
     input_number = input("Enter the number of your choice for operation: ")
     if input_number == "1":
-
         personal_info()
         cart = []
         cart = select_laptop(cart)
@@ -29,23 +28,14 @@ while user_input:
                 while ship:
                     ship_or_not = input("Do you want to ship?(y/n): ")
                     print("\n")
+                    obj_datetime = datetime.now()
                     if ship_or_not == "y":
-                        obj_datetime = datetime.now()
-                        date = obj_datetime.date()
-                        print("\t\t\t\t" + " Sales Invoice" + "\n")
-                        print("\t\t\t\t" + "  TECHNO ZONE" + "\n")
-                        print("\t\t\t\t" + "    Kamaladi"+"\n")
-                        print("\t\t\t" + "9841123456,Email:technozone@gmail.com"+"\n")
-                        print("-----------------------------------------------------------------------------------\n")
-                        print("Party Details:"+"\t\t\t\t\t\t"+"     Bill Date:",date,"\n"+"Mega Bank"+"\n")
-                        print("-----------------------------------------------------------------------------------\n")
-                        print(cart)
+                        bill(cart,obj_datetime,ship_or_not)
                         ship = False
                     elif ship_or_not == "n":
-                        print("not shipped")
+                        bill(cart,obj_datetime,ship_or_not)
                         ship = False
                 buy_more = False     
-        #break main loop
         user_input = False
     elif input_number == "3":
         exit()
