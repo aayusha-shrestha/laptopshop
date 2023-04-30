2e3a5e519270727581660c8c5f3d584558f8656b
Aayusha <aayyushashrestha@gmail.com>
2023-04-30 18:08:30 +0545

    completed bill

--- operations.py
+++ operations.py
@@ -33,51 +33,84 @@ def display_operation_num():
     print("enter 3. To exit"+"\n")
 
 def personal_info():
-            """ask user their name, contact, address"""
-            print("\n")
-            customer_name = input("Enter your name/company's name: ")
-            print("\n")
-            print("Hi "+customer_name+"! Welcome to Techno Zone."+"\n"+"Take a look at our laptops and decide which one you want."+"\n"+
-              "Before proceeding further we will be asking for your personal details for the bill.")
-            print("\n")
-            customer_contact = input("Enter your contact no.: ")
-            print("\n")
-            customer_address = input("Enter your address: ")
-            print("\n")
+    """ask user their name, contact, address"""
+    print("\n")
+    customer_name = input("Enter your name/company's name: ")
+    print("\n")
+    print("Hi "+customer_name+"! Welcome to Techno Zone."+"\n"+"Take a look at our laptops and decide which one you want."+"\n"+
+        "Before proceeding further we will be asking for your personal details for the bill.")
+    print("\n")
+    customer_contact = input("Enter your contact no.: ")
+    print("\n")
+    customer_address = input("Enter your address: ")
+    print("\n")
 
 def select_laptop(cart):
-            laptops_2d_lst = get_laptops()
-            #ask for laptop id
-            customer_laptop_id = int(input("Enter laptop id of the laptop you would like to purchase: "))
-            print("\n")
-            while customer_laptop_id <= 0 or customer_laptop_id > len(laptops_2d_lst):
-                print("INVALID INPUT"+"\n")
-                display_laptops()
-                customer_laptop_id = int(input("Enter laptop id of the laptop you would like to purchase: "))
-                print("\n")
+    inventory = get_laptops()
+    #ask for laptop id
+    customer_laptop_id = int(input("Enter laptop id of the laptop you would like to purchase: "))
+    print("\n")
+    while customer_laptop_id <= 0 or customer_laptop_id > len(inventory):
+        print("INVALID INPUT"+"\n")
+        display_laptops()
+        customer_laptop_id = int(input("Enter laptop id of the laptop you would like to purchase: "))
+        print("\n")
 
-            #update laptop quantity in 2d list
-            customer_qty = int(input("Enter the number of laptops you would you like to purchase: "))
-            print("\n")
-            current_qty = int(laptops_2d_lst[customer_laptop_id-1][3])
-            while customer_qty <= 0 or customer_qty > current_qty:
-                print("INVALID QUANTITY"+"\n")
-                customer_qty = int(input("Enter the number of laptops you would you like to purchase: "))
-                print("\n")
-            laptops_2d_lst[customer_laptop_id-1][3] = str(current_qty - customer_qty)
+    #update laptop quantity in 2d list
+    customer_qty = int(input("Enter the number of laptops you would you like to purchase: "))
+    print("\n")
+    current_qty = int(inventory[customer_laptop_id-1][3])
+    while customer_qty <= 0 or customer_qty > current_qty:
+        print("INVALID QUANTITY"+"\n")
+        customer_qty = int(input("Enter the number of laptops you would you like to purchase: "))
+        print("\n")
+    inventory[customer_laptop_id-1][3] = str(current_qty - customer_qty)
 
-            #update laptop quantity in txt file
-            with open("availableLaptops.txt","w") as laptop_file:
-                for list in laptops_2d_lst:
-                    list_to_string = ','.join(list)
-                    laptop_file.write(list_to_string)
+    #update laptop quantity in txt file
+    with open("availableLaptops.txt","w") as laptop_file:
+        for list in inventory:
+            laptop_file.write(','.join(list))
             
-            #cart
-            customer_product = laptops_2d_lst[customer_laptop_id-1][0]
-            cost_price_dollar = laptops_2d_lst[customer_laptop_id-1][2]
-            cost_price = laptops_2d_lst[customer_laptop_id-1][2].replace("$","")
-            total_price = int(cost_price)*customer_qty
+    #cart
+    customer_product = inventory[customer_laptop_id-1][0]
+    brand = inventory[customer_laptop_id-1][1]
+    rate = inventory[customer_laptop_id-1][2]
+    cost_price = inventory[customer_laptop_id-1][2].replace("$","")
+    net_amount = int(cost_price)*customer_qty
 
-            cart.append([customer_product,customer_qty,cost_price_dollar,total_price])
-            print(cart)
-            return cart
+    cart.append([customer_product,brand,str(customer_qty),rate,str(net_amount)])
+    print(cart)
+    return cart
+
+def bill(cart,obj_datetime,ship_or_not):
+    date = obj_datetime.date()
+    time = obj_datetime.time()
+    print("\t\t\t\t" + " Sales Invoice" + "\n")
+    print("\t\t\t\t" + "  TECHNO ZONE" + "\n")
+    print("\t\t\t\t" + "    Kamaladi"+"\n")
+    print("\t\t\t" + "9841123456,Email:technozone@gmail.com"+"\n")
+    print("---------------------------------------------------------------------------------------\n")
+    print("Party Details:","\t\t\t\t\t\t","Bill Date:",date,"\n"+"Mega Bank","\t\t\t\t\t\t","Bill Time:",time,"\n")
+    print("---------------------------------------------------------------------------------------\n")
+    print("Particulars\t\t"+"Brand\t\t\t"+"Qty\t\t"+"Net Rate\t"+"NetAmt\t")
+    print("---------------------------------------------------------------------------------------\n")
+    for each in cart:
+        print("\t\t".join(each))
+        print("\n")
+    print("---------------------------------------------------------------------------------------\n")
+    total = 0
+    for each in cart:
+        total = total + int(each[4])
+    print("Total:\t\t\t\t\t\t\t\t\t\t"+str(total)+"\n")
+    if ship_or_not == "y":
+        shipping_charge = 100
+        print("Freight & Forwarding Charges:\t\t\t\t\t\t\t"+str(shipping_charge)+"\n")
+        print("---------------------------------------------------------------------------------------\n")
+        grand_total = total + shipping_charge
+        print("Grand Total:\t\t\t\t\t\t\t\t\t"+str(grand_total))
+    print("---------------------------------------------------------------------------------------\n")
+    print("Terms & Conditions\n")
+    print("1.Goods once sold will not be returned.")
+    print("2.We offer complimentary repair services for our products upto 1 year with receipt.\n")
+    print("\t\t\t\tT H A N K  Y O U")
+    print("---------------------------------------------------------------------------------------\n")
