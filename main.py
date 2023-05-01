9dca4946d01230a4f0a52b66bbcd12f6fbcbe5de
Aayusha <aayyushashrestha@gmail.com>
2023-05-01 14:23:27 +0545

    added vat

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from datetime import datetime
 from operations import welcome_msg,display_laptops,display_operation_num,personal_info,select_laptop,bill
 
 welcome_msg()
@@ -7,14 +6,12 @@ display_laptops()
 
 display_operation_num()
 
-user_input = True
-while user_input:
-    input_number = input("Enter the number of your choice for operation: ")
-    if input_number == "1":
+cart = []
+while True:
+    operation_num = input("Enter the number of your choice for operation: ")
+    if operation_num == "1":
         personal_info()
-        cart = []
-        cart = select_laptop(cart)
-
+        cart = select_laptop(operation_num,cart)
         #ask if more laptop needed
         buy_more = True
         while buy_more: 
@@ -22,20 +19,39 @@ while user_input:
             print("\n")
             if user_answer == "y":
                 display_laptops()
-                select_laptop(cart)
+                select_laptop(operation_num,cart)
             elif user_answer == "n":
                 ship = True
                 while ship:
-                    ship_or_not = input("Do you want to ship?(y/n): ")
+                    ship_choice = input("Do you want to ship?(y/n): ")
                     print("\n")
-                    obj_datetime = datetime.now()
-                    if ship_or_not == "y":
-                        bill(cart,obj_datetime,ship_or_not)
+                    if ship_choice == "y":
+                        bill(cart,ship_choice,operation_num)
+                        display_operation_num()
+                        cart = []
                         ship = False
-                    elif ship_or_not == "n":
-                        bill(cart,obj_datetime,ship_or_not)
+                    elif ship_choice == "n":
+                        bill(cart,ship_choice,operation_num)
+                        display_operation_num()
+                        cart = []
                         ship = False
                 buy_more = False     
-        user_input = False
-    elif input_number == "3":
+    elif operation_num == "2":
+        personal_info()
+        cart = select_laptop(operation_num,cart)
+        #ask if more laptop needed
+        buy_more = True
+        while buy_more: 
+            user_answer = input("Do you want to buy more?(y/n): ")
+            print("\n")
+            if user_answer == "y":
+                display_laptops()
+                select_laptop(operation_num,cart)
+            elif user_answer == "n":
+                ship_choice = "n"
+                bill(cart,ship_choice,operation_num)
+                display_operation_num()
+                cart = []
+                buy_more = False     
+    elif operation_num == "3":
         exit()
