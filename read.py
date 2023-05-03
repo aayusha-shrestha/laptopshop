df8aaa78e26bae61e251bf21ec52a31f87228c15
Aayusha <aayyushashrestha@gmail.com>
2023-05-03 22:37:55 +0545

    handled exception file handling

--- read.py
+++ read.py
@@ -1,11 +1,16 @@
 def display_laptops():
-    """reads text file containing laptops and displays it"""
-    with open("read.txt","r") as laptop_file:
-        print("="*132 + "\n")
-        print("LAPTOP ID\t"+"NAME\t\t\t"+"BRAND\t\t\t"+"PRICE\t\t"+"QUANTITY\t"+"PROCESSOR\t\t"+"GRAPHIC CARD"+"\n")
-        print("="*132 + "\n")
-        laptop_id = 1
-        for line in laptop_file:
-            print(laptop_id,"\t\t" + line.replace(",","\t\t"))
-            laptop_id += 1
-        print("\n" + "="*132 + "\n")
+    """reads text file containing laptops and prints it"""
+    try:
+        with open("read.txt","r") as laptop_file:
+            print("="*132 + "\n")
+            print("LAPTOP ID\t"+"NAME\t\t\t"+"BRAND\t\t\t"+"PRICE\t\t"+"QUANTITY\t"+"PROCESSOR\t\t"+"GRAPHIC CARD"+"\n")
+            print("="*132 + "\n")
+            laptop_id = 1
+            for line in laptop_file:
+                print(laptop_id,"\t\t" + line.replace(",","\t\t"))
+                laptop_id += 1
+            print("\n" + "="*132 + "\n")
+    except FileNotFoundError:
+        print("File not found")
+    except Exception:
+        print("An error occured")
