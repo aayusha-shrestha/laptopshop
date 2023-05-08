b1ae3de2586996e29cfe5c8f496db0aa2c8fc10b
Aayusha <aayyushashrestha@gmail.com>
2023-05-08 15:07:02 +0545

    handled out of stock

--- read.py
+++ read.py
@@ -1,3 +1,17 @@
+def get_laptops():
+    """reads txt file containing laptops and stores it in 2d list"""
+    laptops_2d_lst = []
+    try:
+        with open("read.txt","r") as laptop_file:
+            for line in laptop_file:
+                inner_lst = line.split(",") #split the line on runs of commas
+                laptops_2d_lst.append(inner_lst)
+        return laptops_2d_lst
+    except FileNotFoundError:
+        print("File not found")
+    except Exception:
+        print("An error occured")
+
 def display_laptops():
     """reads text file containing laptops and prints it"""
     try:
