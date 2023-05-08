5c818ab3a1b2cdde4d8ea02e010e665512306a15
Aayusha <aayyushashrestha@gmail.com>
2023-05-08 15:06:57 +0545

    handled out of stock

--- operations.py
+++ operations.py
@@ -1,60 +1,55 @@
-def get_laptops():
-    """reads txt file containing laptops and stores it in 2d list"""
-    laptops_2d_lst = []
-    try:
-        with open("read.txt","r") as laptop_file:
-            for line in laptop_file:
-                inner_lst = line.split(",") #split the line on runs of commas
-                laptops_2d_lst.append(inner_lst)
-        return laptops_2d_lst
-    except FileNotFoundError:
-        print("File not found")
-    except Exception:
-        print("An error occured")
-
-def select_laptop(operation_num,cart):
-    inventory = get_laptops()
+def select_laptop(inventory,operation_num):
     #ask for laptop id
     id_success = False
     while id_success == False:
         try:
             customer_laptop_id = int(input("Enter laptop id of the laptop you would like to purchase: "))
             print("\n")
-            current_qty = int(inventory[customer_laptop_id-1][3])
-            if customer_laptop_id <= 0 or customer_laptop_id > len(inventory):
-                    print("ID DOES NOT EXIST\n")
+            current_qty = int(inventory[customer_laptop_id-1][3])      
+            if operation_num == "1":
+                if customer_laptop_id <= 0 or customer_laptop_id > len(inventory):
+                    print("Error: ID does not exist\n")
                     # display_laptops()
-            elif operation_num == "1":
-                if current_qty == 0:
+                elif current_qty == 0:
                     print("The product is currently out of stock. We apologize for the inconvenience.\n"+
                       "Please feel free to explore some of our other products that may meet your needs. \n")
                 else :
                     id_success = True
-            else:
-                id_success = True
+                    return customer_laptop_id
+            elif operation_num == "2":
+                if customer_laptop_id <= 0 or customer_laptop_id > len(inventory):
+                    print("Error: ID does not exist\n")
+                else:
+                    id_success = True
+                    return customer_laptop_id
         except:
-            print("INVALID input for ID\n")
+            print("Error: Invalid input for ID\n")
 
+def select_qty(inventory,operation_num,cart):
+    customer_laptop_id = select_laptop(inventory,operation_num)
+    current_qty = int(inventory[customer_laptop_id-1][3])
     #ask for laptop quantity
     qty_success = False
     while qty_success == False:
         try:
             customer_qty = int(input("Enter the number of laptops you would you like to purchase: "))
             print("\n")
-            if customer_qty <= 0:
-                print("INVALID QUANTITY\n")
-            elif operation_num == "1":
-                if customer_qty > current_qty:
+            if operation_num == "1":
+                if customer_qty <= 0:
+                    print("Error: Invalid quantity\n")
+                elif customer_qty > current_qty:
                     print("Unfortunately, at the moment we only have",current_qty,"available.\n")
                 else:
                     qty_success = True
             elif operation_num == "2":
-                if customer_qty > 100:
+                if customer_qty <= 0:
+                    print("Error: Invalid quantity\n")
+                elif customer_qty > 100:
                     print("Please note that we are currently limiting orders to 100 per resellers.\n")
                 else:
                     qty_success = True
         except:
-            print("INVALID input for quantity\n")
+            print("Error: Invalid input for quantity\n")
     
     #update laptop quantity in 2d list
     if operation_num == "1":
