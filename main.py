c4dda18d29b07c05ed5ef1f09d9743ff0631d776
Aayusha <aayyushashrestha@gmail.com>
2023-05-11 11:13:49 +0545

    completed main.py

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
-from operations import select_id, select_qty, add_to_cart
+from operations import get_name,select_id, select_qty, add_to_cart
 from read import get_laptops, display_laptops, display_bill
 from write import welcome_msg, display_operation_num, update_qty, bill
 
 welcome_msg()
 display_laptops()
-display_operation_num()
 inventory = get_laptops()
 cart = []
 
 while True:
+    display_operation_num()
     operation_num = input("Enter the number of your choice for operation: ")
     print("\n")
     if operation_num == "1":
-        name = input("Enter name/company's name: ")
-        print("\n")
+        name = get_name()
+        display_laptops()
         id = select_id(inventory,operation_num)
         current_qty = int(inventory[id-1][3])
         qty = select_qty(current_qty,operation_num)
@@ -27,6 +27,7 @@ while True:
             user_answer = input("Would you like another laptop?(y/n): ")
             print("\n")
             if user_answer == "y":
+                display_laptops()
                 id = select_id(inventory,operation_num)
                 current_qty = int(inventory[id-1][3])
                 qty = select_qty(current_qty,operation_num)
@@ -42,21 +43,19 @@ while True:
                         filename = bill(name,cart,ship_choice,operation_num)
                         display_bill(filename)
                         display_laptops()
-                        display_operation_num()
                         cart = []
                         ship = False
                     elif ship_choice == "n":
                         filename = bill(name,cart,ship_choice,operation_num)
                         display_bill(filename)
                         display_laptops()
-                        display_operation_num()
                         cart = []
                         ship = False
                 buy_more = False     
 
     elif operation_num == "2":
-        name = input("Enter name/company's name: ")
-        print("\n")
+        name = get_name()
+        display_laptops()
         id = select_id(inventory,operation_num)
         current_qty = int(inventory[id-1][3])
         qty = select_qty(current_qty,operation_num)
@@ -70,6 +69,7 @@ while True:
             user_answer = input("Do you want to buy more?(y/n): ")
             print("\n")
             if user_answer == "y":
+                display_laptops()
                 id = select_id(inventory,operation_num)
                 current_qty = int(inventory[id-1][3])
                 qty = select_qty(current_qty,operation_num)
@@ -81,9 +81,9 @@ while True:
                 filename = bill(name,cart,ship_choice,operation_num)
                 display_bill(filename)
                 display_laptops()
-                display_operation_num()
                 cart = []
                 buy_more = False    
 
     elif operation_num == "3":
+        print("Thank you for visiting our shop.\n")
         exit()
